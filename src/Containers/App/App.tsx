d524b40d9c9f133fd9661d984154dbfe33bd347c
Jhon Garcia <metalprime57@gmail.com>
2022-02-07 11:32:19 -0500

    connection of API Woocommerce in App.tsx, it´s necessary an update

--- src/Containers/App/App.tsx
+++ src/Containers/App/App.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Product } from '../../Components/Product/Product';
+import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+
+const api = new WooCommerceRestApi({
+    url: "https://aveleri.com/",
+    consumerKey: "ck_cfd56ff458ea681ea638d0da6a62acd6665e9791",
+    consumerSecret: "cs_7624ac89b0c8f36262d420319e0e2b803a63472e",
+    version: "wc/v3",
+    queryStringAuth: true
+  });
 
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  let fetchOrders = () => {
+    api
+      .get("products", {
+        per_page: 20,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setProducts(response.data);
+        }
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+
+      });
+  };
+
+
+
+
   return (
     <div className="App">
+      {
+
+      }
       <Product></Product>
       
     </div>
