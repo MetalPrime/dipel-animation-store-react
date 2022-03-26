901079c4829bf53a1b73248924294221a0b980dc
Jhon Garcia <metalprime57@gmail.com>
2022-03-26 18:32:46 -0500

    creation of filter in Product Selection

--- src/Containers/ProductSelection/ProductSelection.tsx
+++ src/Containers/ProductSelection/ProductSelection.tsx
@@ -4,8 +4,10 @@ import { Animation } from '../../Components/Animation/Animation';
 import { Product } from '../../Components/Product/Product';
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 import imgBackground from '../../Resources/backAnimation.jpg';
+import todoIcon from '../../Resources/todoIcon.svg';
 import { AmountType } from '../../Types/AmountVisual';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export type ProductSelection = {
     scriptLoaded: boolean,
@@ -15,17 +17,63 @@ export type ProductSelection = {
 }
 
 export const ProductSelection: React.FC<ProductSelection> = ({ scriptLoaded, amountVisual, products, setAmountVisual }) => {
+
+    let [copyProducts,setCopyProducts] = useState(products)
+
+    const handleFilter = (productItem: "todos"| "cemento" | "acabados" | "madera" | "acero" | "ladrillos") => {
+        let productsCopy = [...products];
+
+        if(productItem === "todos"){
+            productsCopy = [...products]
+        }
+
+        else {
+            productsCopy = products.filter(product => product.shipping_class === productItem);
+        }
+        setCopyProducts(productsCopy);
+    }
+
     return <section className="ProductSelection">
         <article className="ProductSelection_animation" >
             {scriptLoaded && <Animation amountVisual={amountVisual}></Animation>}
             <section className="ProductSelection_filters">
                 <div className="ProductSelection_filters_container">
-                    <button className="ProductSelection_filter"></button>
-                    <button className="ProductSelection_filter"></button>
-                    <button className="ProductSelection_filter"></button>
-                    <button className="ProductSelection_filter"></button>
-                    <button className="ProductSelection_filter"></button>
-                    <button className="ProductSelection_filter"></button>
+                    <button className="ProductSelection_filter" onClick={()=>{
+                        handleFilter("todos");
+                    }}>
+                        <img src={todoIcon} alt="img_filter_btn" className="ProductSelection_filter_img" />
+                        <p className="ProductSelection_filter_text">Todos</p>
+                    </button>
+                    <button className="ProductSelection_filter" onClick={()=>{
+                        handleFilter("acero");
+                    }}>
+                        <img src="" alt="img_filter_btn" className="ProductSelection_filter_img" />
+                        <p className="ProductSelection_filter_text">Acero</p>
+                    </button>
+                    <button className="ProductSelection_filter" onClick={()=>{
+                        handleFilter("ladrillos");
+                    }}>
+                        <img src="" alt="img_filter_btn" className="ProductSelection_filter_img" />
+                        <p className="ProductSelection_filter_text">Ladrillos</p>
+                    </button>
+                    <button className="ProductSelection_filter" onClick={()=>{
+                        handleFilter("madera");
+                    }}>
+                        <img src="" alt="img_filter_btn" className="ProductSelection_filter_img" />
+                        <p className="ProductSelection_filter_text">Madera</p>
+                    </button>
+                    <button className="ProductSelection_filter" onClick={()=>{
+                        handleFilter("acabados");
+                    }}>
+                        <img src="" alt="img_filter_btn" className="ProductSelection_filter_img" />
+                        <p className="ProductSelection_filter_text">Acabados</p>
+                    </button>
+                    <button className="ProductSelection_filter" onClick={()=>{
+                        handleFilter("cemento");
+                    }}>
+                        <img src="" alt="img_filter_btn" className="ProductSelection_filter_img" />
+                        <p className="ProductSelection_filter_text">Cemento</p>
+                    </button>
                 </div>
 
             </section>
@@ -33,7 +81,7 @@ export const ProductSelection: React.FC<ProductSelection> = ({ scriptLoaded, amo
 
         <article className="ProductSelection_products">
             {
-                scriptLoaded && products.map(product => {
+                scriptLoaded && copyProducts.map(product => {
                     return <Product name={product.name} img={product.images[0].src} type={product.type} value={product.price} weight={product.weight} key={product.id} amountVisual={amountVisual} setAmountVisual={setAmountVisual} id={product.id} shipping_class={product.shipping_class} price={product.price}></Product>
                 })
             }
