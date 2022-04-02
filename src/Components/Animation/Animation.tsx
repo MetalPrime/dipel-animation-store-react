8c794dff63c55d8930ecc24d32b6a723fbb0494b
Jhon Garcia <metalprime57@gmail.com>
2022-04-02 02:01:22 -0500

    update in elements of animation

--- src/Components/Animation/Animation.tsx
+++ src/Components/Animation/Animation.tsx
@@ -3,9 +3,10 @@ import './Animation.css'
 import Sketch from "react-p5";
 import p5Types from "p5"; //Import this for typechecking and intellisense
 import { AmountType } from "../../Types/AmountVisual";
-import truckLateralItem from "../../Resources/Truck_Final_Lateral.svg";
-import truckFrontItem from "../../Resources/Truck_Final_Front.svg";
+import truckLateralItem from "../../Resources/Truck_Final_Lateral.png";
+import truckFrontItem from "../../Resources/Truck_Final_Front.png";
 import cement from "../../Resources/cement.svg";
+import road from "../../Resources/carretera@4x.png";
 
 export type Animation = {
 	//Your component props
@@ -22,18 +23,24 @@ export const Animation: React.FC<Animation> = ({amountVisual}) => {
 	let imgTruckLateralURL = truckLateralItem;
 	let imgTruckFrontalURL = truckFrontItem;
 	let imgCementURL = cement;
+	let imgRoadURL = road;
 	let canvas;
 
 
 	let imgTruckLateral: p5Types.Image ;
 	let imgCement : p5Types.Image ;
-	let imgTruckFront: p5Types.Image
+	let imgTruckFront: p5Types.Image;
+	let imgRoad : p5Types.Image ;
+
+	let bgPosX : number = 0
+	let bgPosY : number = 0
 
 	useEffect(() => {
 		visualObjects = {...visualObjects, amountVisual}
 		imgTruckLateralURL = truckLateralItem;
 		imgTruckFrontalURL = truckFrontItem;
 		imgCementURL = cement;
+		imgRoadURL = road;
 	},[amountVisual])
 
 	const preload = (p5: p5Types) => {
@@ -45,6 +52,8 @@ export const Animation: React.FC<Animation> = ({amountVisual}) => {
 			imgCement = img;
 		});
 		imgCement = p5.loadImage(cement);
+
+		imgRoad = p5.loadImage(imgRoadURL);
 	}
     
 	//See annotations in JS for more information
@@ -53,10 +62,13 @@ export const Animation: React.FC<Animation> = ({amountVisual}) => {
 		
 		console.log(imgTruckLateral);
 		console.log(imgCement);
+
+		
+		
 	};
 
 	const draw = (p5: p5Types) => {
-		p5.background(255);
+		p5.background(72, 111, 56);
 		if (imgCementURL) {
 			imgCement = p5.loadImage(imgCementURL, (img) => imgCement = img)
 			imgCementURL = '';
@@ -71,12 +83,23 @@ export const Animation: React.FC<Animation> = ({amountVisual}) => {
 			imgTruckFront = p5.loadImage(imgTruckFrontalURL, (img) => imgTruckFront = img)
 			imgTruckFrontalURL = '';
 		}
+
+		if (imgRoadURL) {
+			imgRoad = p5.loadImage(imgRoadURL, (img) => imgRoad = img)
+			imgRoadURL = '';
+		}
 		
 		p5.fill(255);
 
-		if(imgTruckLateral){
-			p5.image(imgTruckLateral, 100,100)
+		if(imgRoad){
+			p5.image(imgRoad,bgPosX,bgPosY, window.innerWidth * 1.6,1200);
 		}
+
+		if(imgTruckLateral){
+			p5.image(imgTruckLateral, 400,50,800,400);
+		}
+
+
 		
         visualObjects.amountVisual.forEach((object,i) =>{
             for (let index = 0; index < object.amount; index++) {
@@ -85,19 +108,43 @@ export const Animation: React.FC<Animation> = ({amountVisual}) => {
 					p5.image(imgCement,x+(i*50), y+(index*50),50,50);
 					
 				}
+
+				if(object.amount >10){
+					moveAnimation();
+				}
 				
             }
             
         })
 
-		if(imgTruckLateral){
-			p5.image(imgTruckFront, 250,130)
+		if(imgTruckFront){
+			p5.image(imgTruckFront, 850,160,380,280)
 		}
 
 		
 	};
 
-	return <Sketch preload={preload}setup={setup} draw={draw}  />;
+	const mouseClicked = (p5: p5Types) => {
+		bgPosY -= 4.8;
+		bgPosX -= 10;
+		if(bgPosX<-690){
+			bgPosX = 0;
+			bgPosY = 0;
+		}
+		
+	}
+
+	const moveAnimation = () => {
+		bgPosY -= 0.45;
+		bgPosX -= 0.97;
+		if(bgPosX<-690){
+			bgPosX = 0;
+			bgPosY = 0;
+		}
+	}
+	
+
+	return <Sketch preload={preload}setup={setup} draw={draw} mouseClicked={mouseClicked}  />;
 };
 
 
